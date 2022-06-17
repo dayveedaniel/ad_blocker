8725ef1297d1f7c1eec5f1dbb2efc21bc5da1294
dayveed <dayveed.d@gmail.com>
2022-06-17 08:37:25 +0700

    added more utils

--- lib/ad_blocker_app.dart
+++ lib/ad_blocker_app.dart
@@ -1,4 +1,4 @@
-
+import 'package:ad_blocker/ui/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
