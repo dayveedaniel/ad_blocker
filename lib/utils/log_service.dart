7c1fff5fd5fde654b5bbbbd01982a717e5516167
dayveed <dayveed.d@gmail.com>
2022-06-18 21:06:45 +0700

    utils added

---
+++ lib/utils/log_service.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../di/injector.dart';
+
+void println(String text, {String? tag}) {
+  getIt.get<LogService>()._log(text, tag);
+}
+
+@Injectable(as: LogService, env: [Environment.dev, Environment.test])
+class LogServiceDev extends LogService {
+  LogServiceDev() : super(Environment.dev);
+}
+
+@Injectable(as: LogService, env: [Environment.prod])
+class LogServiceProd extends LogService {
+  LogServiceProd() : super(Environment.prod);
+}
+
+abstract class LogService {
+  final String _env;
+
+  LogService(@factoryParam this._env);
+
+  void _log(String text, [String? tag]) {
+    if (kDebugMode && _env != Environment.prod) {
+      tag == null ? print(text) : print("$tag => $text");
+    }
+  }
+}
